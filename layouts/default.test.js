70b04cb8367f36438caf3f5b3f8c3972794cb04a
Gabriel Medina <gmedina.santos@gmail.com>
2021-10-10 20:31:57 -0400

    add test to query search

--- layouts/default.test.js
+++ layouts/default.test.js
@@ -5,13 +5,16 @@ const mockRouter = {
   push: jest.fn()
 }
 
-function renderComponent() {
+function renderComponent(params) {
+  const { $route } = params
+
   const { container } = render(LayoutDefault, {
     stubs: {
       Nuxt: true,
       NuxtLink: true,
     },
     mocks: {
+      $route,
       $router: mockRouter,
     },
   })
@@ -23,18 +26,45 @@ function renderComponent() {
 
 describe('Layouts > Default', () => {
   describe('when the layout is rendered', () => {
+    const $route = {
+      path: '/',
+      query: {}
+    }
+
     it('should render correctly', () => {
-      const { container } = renderComponent()
+      const { container } = renderComponent({ $route })
 
       expect(container).toBeDefined()
+    })
+  })
+
+  describe('when url is provide with query search', () => {
+    const term = 'term'
+
+    const $route = {
+      path: '/',
+      query: {
+        search: term
+      }
+    }
+
+    it('should render the searchbox with value', () => {
+      renderComponent({ $route })
+
+      expect(screen.getByRole('searchbox').value).toEqual(term)
     })
   })
 
   describe('when user input data on searchbox', () => {
     const term = 'term'
 
+    const $route = {
+      path: '/',
+      query: {}
+    }
+
     it('should push router to "/" with query search value', async () => {
-      renderComponent()
+      renderComponent({ $route })
 
       await fireEvent.update(screen.getByRole('searchbox'), term)
 
