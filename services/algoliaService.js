d06ee2147b5bf1e5f919362bd0aa8f78296e2d75
Gabriel Medina <gmedina.santos@gmail.com>
2021-10-14 18:24:36 -0400

    fix algolia process env

--- services/algoliaService.js
+++ services/algoliaService.js
@@ -3,8 +3,8 @@ import algoliasearch from 'algoliasearch'
 class AlgoliaService {
   constructor() {
     this._client = algoliasearch(
-      process.env.ALGOLIA_APPLICATION_ID || process.env.algolia.applicationId,
-      process.env.ALGOLIA_API_KEY || process.env.algolia.apiKey
+      process.env.ALGOLIA_APPLICATION_ID || process.env.algolia?.applicationId,
+      process.env.ALGOLIA_API_KEY || process.env.algolia?.apiKey
     )
   }
 
