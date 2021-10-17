5827b9e87585acbdfad8c3adab765c74b8c3d690
Gabriel Medina <gmedina.santos@gmail.com>
2021-10-16 22:46:52 -0400

    add focus recovery

--- managers/domManager.js
+++ managers/domManager.js
@@ -1,10 +1,38 @@
 class DomManager {
+  constructor() {
+    this._elementTriggerFocus = null
+  }
+
+  // body scroll locker
   disableBodyScroll(element) {
     element.classList.add('body--scroll-lock')
   }
 
   enableBodyScroll(element) {
     element.classList.remove('body--scroll-lock')
+  }
+
+  // element focus return
+  setTriggerElementFocus(element) {
+    this._elementTriggerFocus = element
+  }
+
+  getTriggerElementFocus() {
+    return this._elementTriggerFocus
+  }
+
+  returnFocusToTriggerElement() {
+    const element = this._elementTriggerFocus
+
+    if (element) {
+      element.focus()
+
+      this._elementTriggerFocus = null
+
+      return true
+    }
+
+    return false
   }
 }
 
