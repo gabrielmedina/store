853bf68a5f6a40f43b1e07b25d67129e02660a30
Gabriel Medina <gmedina.santos@gmail.com>
2021-11-22 09:17:51 -0400

    remove search methods from productManager

--- src/managers/productManager.js
+++ src/managers/productManager.js
@@ -1,40 +1,10 @@
-import algoliaService from '@/services/algoliaService'
 import Products from '@/assets/data/products'
 
 class ProductManager {
-  async get() {
-    algoliaService.setIndex('dev_store')
-
-    try {
-      const response = await algoliaService.get()
-
-      return {
-        products: response.hits || []
-      }
-    } catch (error) {
-      console.log(error) // eslint-disable-line
-    }
-  }
-
   getById(id) {
     const product = Products.find((product) => product.id === id)
-    // const product = await api.getProduct({ id })
 
     return product
-  }
-
-  async getByTerm(term) {
-    algoliaService.setIndex('dev_store')
-
-    try {
-      const response = await algoliaService.getByTerm(term)
-
-      return {
-        products: response.hits || []
-      }
-    } catch (error) {
-      console.log(error) // eslint-disable-line
-    }
   }
 }
 
