ed17c3796b77aaa391a6cb88176e0ba708817446
= <gmedina.santos@gmail.com>
2022-02-28 13:44:42 -0400

    test: get product info from Algolia

--- test/src/managers/productManager.test.js
+++ test/src/managers/productManager.test.js
@@ -1,14 +1,29 @@
 import Products from '@@/test/_stubs/products'
 import productManager from '@/managers/productManager'
+import algoliaService from '@/services/algoliaService'
+
+jest.mock('@/services/algoliaService')
+
+algoliaService.getByTerm = jest.fn().mockImplementation(() => {
+  return Promise.resolve({
+    hits: [{ ...Products[0] }]
+  })
+})
 
 describe('Managers > ProductManager', () => {
   afterEach(() => {
     jest.clearAllMocks()
   })
 
-  it('when getById() is called', () => {
+  it('when getById() is called', async () => {
     const product = Products[0]
 
-    expect(productManager.getById(product.id)).toEqual(product)
+    const response = await productManager.getById(product.id)
+
+    expect(algoliaService.setIndex).toBeCalledTimes(1)
+    expect(algoliaService.setIndex).toBeCalledWith('dev_store')
+    expect(algoliaService.getByTerm).toBeCalledTimes(1)
+    expect(algoliaService.getByTerm).toBeCalledWith(product.id)
+    expect(response).toEqual(product)
   })
 })
